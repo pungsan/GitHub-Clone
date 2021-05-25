adf4253da82f1d489d6a3fe28dc843775b397e84
pungsan\whypungsan <whypungsan@gmail.com>
2021-05-25 09:10:16 +0900

    GitHub Clone

---
+++ main.js
@@ -0,0 +1,26 @@
+(function (window, document) {
+    'use strict';
+
+    const $toggles = document.querySelectorAll('.toggle'); // NodeList (유사배열)
+    const $toggleBtn = document.getElementById('toggle-btn');
+
+    $toggleBtn.addEventListener('click', function () {
+        toggleElements();
+    });
+
+    window.addEventListener('resize', function () {
+        offElements();
+    })
+
+    function toggleElements() {
+        [].forEach.call($toggles, function (toggle) {
+            toggle.classList.toggle('on');
+        });
+    }
+
+    function offElements() {
+        [].forEach.call($toggles, function (toggle) {
+            toggle.classList.remove('on');
+        });
+    }
+})(window, document)
